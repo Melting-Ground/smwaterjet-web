a56d3585577c1e7936b5077a74eae3d512807604
UiChan Lee <junglkls1234@gmail.com>
2024-09-20 17:35:24 +0900

    Feat: admins 테이블

--- backend/migrations/20240920082342_create_admins_table.js
+++ backend/migrations/20240920082342_create_admins_table.js
@@ -3,7 +3,11 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('admins', function(table) {
+        table.increments('id').primary(); 
+        table.string('phone_number').unique().notNullable(); 
+        table.string('password_hash').notNullable();
+    });
 };
 
 /**
@@ -11,5 +15,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('admins'); 
 };
