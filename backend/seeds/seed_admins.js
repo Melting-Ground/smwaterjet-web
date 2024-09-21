4967f3c6b8f91f8bc7ad19bdd4cf60c541716777
UiChan Lee <junglkls1234@gmail.com>
2024-09-21 16:14:38 +0900

    Chore: argon2 설치

--- backend/seeds/seed_admins.js
+++ backend/seeds/seed_admins.js
@@ -4,10 +4,9 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('admins').del()
+  await knex('admins').insert([
+    {id: 1, phone_number: '', password_hash:""},
+    {id: 1, phone_number: '', password_hash:"" }
   ]);
 };
