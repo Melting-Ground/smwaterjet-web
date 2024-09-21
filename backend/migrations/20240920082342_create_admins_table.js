d40f177e9bafaccbb6a4189696e66828cc313e3a
UiChan Lee <junglkls1234@gmail.com>
2024-09-21 14:45:36 +0900

    Feat: reports 테이블

--- backend/migrations/20240920082342_create_admins_table.js
+++ backend/migrations/20240920082342_create_admins_table.js
@@ -5,7 +5,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('admins', function(table) {
         table.increments('id').primary(); 
-        table.string('phone_number').unique().notNullable(); 
+        table.string('phone_number',11).unique().notNullable(); 
         table.string('password_hash').notNullable();
     });
 };
