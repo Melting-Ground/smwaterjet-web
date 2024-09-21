5b52b9db21da43df25e9184ae542a40a64140dcc
UiChan Lee <junglkls1234@gmail.com>
2024-09-21 16:22:20 +0900

    Feat: admins seed 생성

--- backend/seeds/seed_admins.js
+++ backend/seeds/seed_admins.js
@@ -1,3 +1,6 @@
+const argon2 = require('argon2');
+require('dotenv').config();
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
@@ -5,8 +8,11 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('admins').del()
+
+  const hashedPassword = await argon2.hash(process.env.INITIAL_PASSWORD);
+
   await knex('admins').insert([
-    {id: 1, phone_number: '', password_hash:""},
-    {id: 1, phone_number: '', password_hash:"" }
+    {phone_number: process.env.NUMBER_CHAN, password_hash: hashedPassword},
+    {phone_number: process.env.NUMBER_MASTER, password_hash: hashedPassword}
   ]);
 };
