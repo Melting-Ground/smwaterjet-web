b4d814a9d7cdde21effdd20503aa23eefc65b8a1
UiChan Lee <junglkls1234@gmail.com>
2024-10-07 14:36:19 +0900

    Chore: test 관련 설정 추가

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,26 @@ module.exports = {
       directory: './seeds'
     }
   },
-
+  test: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
   staging: {
     client: 'mysql2',
     connection: {
