f5a6e3a82bea6e1b70e700c59442f6e4d6c9752b
UiChan Lee <junglkls1234@gmail.com>
2024-11-06 18:40:28 +0900

    Chore: photo table 변경

--- src/main/models/photo.js
+++ src/main/models/photo.js
@@ -1,7 +1,6 @@
 class Photo {
-    constructor({ title, content, year, path }) {
+    constructor({ title, year, path }) {
         this.title = title;
-        this.content = content;
         this.year = year;
         this.path = path;
     }
