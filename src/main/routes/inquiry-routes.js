711b7524a6bfbcacb8f840b28710999ef11ce93f
UiChan Lee <junglkls1234@gmail.com>
2024-11-09 16:07:42 +0900

    Fix: 사용자 인증 기능을 위해 post로 변경

--- src/main/routes/inquiry-routes.js
+++ src/main/routes/inquiry-routes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/', InquiryController.getAllInquiries);
 router.get('/search', InquiryController.searchInquiries);
 router.get('/admin/:inquiryId', authenticate, InquiryController.getInquiryById);
-router.get('/:inquiryId', userAuthenticate, InquiryController.getInquiryById);
+router.post('/:inquiryId', userAuthenticate, InquiryController.getInquiryById);
 
 router.post('/', upload.array('files', 5), InquiryController.createInquiry);
 
