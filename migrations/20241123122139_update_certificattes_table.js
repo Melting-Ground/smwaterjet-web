5e1a52d5245070a2515aaaa808faebaec3d3d7e7
UiChan Lee <junglkls1234@gmail.com>
2024-11-23 22:19:55 +0900

    Chore: 인증 테이블 제목 추가

---
+++ migrations/20241123122139_update_certificattes_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.alterTable('certificates', (table) => {
+		table.string('title').notNullable();
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.alterTable('certificates', (table) => {
+		table.dropColumn('title');
+	});
+};
