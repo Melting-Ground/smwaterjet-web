106745010804d323e072480be979fc14d6235e0d
UiChan Lee <junglkls1234@gmail.com>
2024-12-02 20:29:18 +0900

    Feat: password query로 입력

--- src/main/middlewares/user-authentication.js
+++ src/main/middlewares/user-authentication.js
@@ -5,15 +5,20 @@ const Exception = require('@exceptions/exception');
 const userAuthenticate = async (req, res, next) => {
     try {
         const { inquiryId } = req.params;
-        const { password } = req.body;
+        const { password } = req.query;
+        if (password == null) {
+            throw new Exception('BadRequestException', 'Password query parameter is required');
+        }
+
         const user = await db('inquiries').where({ id: inquiryId }).select('password').first();
 
-        if (!user) {
+        if (user == null) {
             throw new Exception('ValueNotFoundException', 'Inquiry is not found');
         }
+
         const isPasswordValid = await argon2.verify(user.password, password);
 
-        if (!isPasswordValid) {
+        if (isPasswordValid == null) {
             throw new Exception('AuthenticationException', 'Invalid password');
         }
         next();
